b307565eea857101a1402a43fde6fd22894cdf18
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-05-25 22:12:26 +0500

    feat: Add chat fetching functionality and update state management in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,49 @@
 "use client"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { contacts, messages as initialMessages, Message } from "@/app/data";
 import ContactList from "@/components/ContactList";
 import Chat from "@/components/Chat";
 
 
+export interface ChatContact {
+  id: number;
+  whatsapp_name: string;
+  custom_whatsapp_name: string;
+  phone_number: string;
+  business: number;
+  customer: number;
+  super_admin: number;
+  agent: number | null;
+  unread_count: number;
+  last_message_at: string | null;
+}
+
 export default function Home() {
+  const [chats, setChats] = useState<ChatContact[]>([]);
   const [selectedId, setSelectedId] = useState(contacts[0].id);
   const [messages, setMessages] = useState<Message[]>(initialMessages);
+  
 
   const handleSelect = (id: string) => setSelectedId(id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/api/v1/whatsapp/chats/", {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          business_id: 5
+        })
+      })
+      if(res.ok) {
+        const data = await res.json();
+        setChats(data.chats);
+        if (data.chats.length > 0) setSelectedId(data.chats[0].id);
+      }
+      
+    }
+    fetchData()
+  }, [])
 
   const handleSend = (text: string) => {
     const newMsg: Message = {
