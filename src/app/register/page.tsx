e652af1071f4aa8c35335183e648a3224fadb79b
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-05-25 13:10:25 +0500

    feat: Enhance registration process with user session handling and additional input fields

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
 
 export default function Register() {
   const [orgName, setOrgName] = useState("");
   const [whatsappNumber, setWhatsappNumber] = useState("");
+  const [wabaId, setWabaId] = useState("")
   const [loading, setLoading] = useState(false);
+  const [userId, setUserId] = useState(null)
   const router = useRouter();
+  const {data: session} = useSession()
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!session?.user?.email) return;
+      const res = await fetch(
+        `http://localhost:8000/api/v1/auth/super-admin/email/${session.user.email}/get`, {
+          method: "GET",
+          headers: {"Content-Type": "application/json"}
+        }
+      );
+      if (res.ok) {
+        const data = await res.json()
+        setUserId(data?.id);
+      }
+    };
+    
+    fetchData();
+  }, [session]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
 
     // Send data to backend API
-    const res = await fetch("/api/register", {
+    const res = await fetch(`http://localhost:8000/api/v1/business/create`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ orgName, whatsappNumber }),
+      body: JSON.stringify({ name:orgName, phone_number:whatsappNumber, waba_id:wabaId, super_admin:userId }),
     });
 
-    setLoading(false);
+    const data = await res.json()
+    console.log("Backend response",data)
+    console.log("Status", data?.status)
 
     if (res.ok) {
+      
       router.push("/dashboard");
     } else {
       alert("Registration failed. Please try again.");
@@ -61,7 +88,20 @@ export default function Register() {
             onChange={(e) => setWhatsappNumber(e.target.value)}
             required
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-gray-700 dark:text-white"
-            placeholder="e.g. 923001234567"
+            placeholder="e.g. +923001234567"
+          />
+        </div>
+         <div>
+          <label className="block mb-1 text-gray-700 dark:text-gray-200">
+            Whatsapp Business Id
+          </label>
+          <input
+            type="int"
+            value={wabaId}
+            onChange={(e) => setWabaId(e.target.value)}
+            required
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-gray-700 dark:text-white"
+            placeholder="Enter organization number id"
           />
         </div>
         <button
